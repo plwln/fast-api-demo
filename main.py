fc2c70160ccc3e39660f2749b2f27d9c50dc022b
plwln <45865754+plwln@users.noreply.github.com>
2020-07-20 17:03:39 +0400

    fixed

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def unzip_page(item: Item):
 async def image_endpoint(images: imageRequest):
     # Returns a cv2 image array from the document vector
     # img = Image.open('./'+images.name+"/image.B2.jpeg")
-    folder = 'shots/' if folder=='empty' else folder
+    folder = 'shots/' if images.folder=='empty' else images.folder
     path = [n for n in os.listdir(folder+images.name+"/") if images.band in n and images.type in n]
     return FileResponse(folder+images.name+"/"+path[0])
 
